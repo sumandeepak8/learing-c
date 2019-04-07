3f0c804def12eca0b4ee3c92c9a68452af069b91
suman rajput <44019903+sumandeepak8@users.noreply.github.com>
2019-04-07 13:24:08 +0530

    Program to include user defined files in header in main

---
+++ include-files/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "sum.h"
+#include "sub.h"
+
+int main(void){
+  printf("the value of sum of a and b is : %d\n", sum(2,3));
+  printf("the value of sub of a and b is : %d\n", sub(3,1));
+  return 0;
+}
